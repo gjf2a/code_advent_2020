0982ab9e9a8840d305ad89b9c92a73f7e39d67a8
gjf2a <ferrer@hendrix.edu>
2020-12-02 08:56:09 -0600

    Password check for day 2_2 passes.

--- src/day2.rs
+++ src/day2.rs
@@ -4,17 +4,31 @@ use std::io::BufRead;
 pub fn solve_1() -> io::Result<String> {
     let mut total = 0;
     for line in io::BufReader::new(fs::File::open("day_2_input.txt")?).lines() {
-        if line_valid_password(line?.as_str()) {
+        if line_valid_password_1(line?.as_str()) {
             total += 1;
         }
     }
     Ok(format!("{}", total))
 }
 
-fn line_valid_password(line: &str) -> bool {
+fn line_valid_password_1(line: &str) -> bool {
     let (lo, hi, letter, password) = parse_password_line(line);
     let count = password.matches(letter).count();
     count >= lo && count <= hi
+}
+
+fn line_valid_password_2(line: &str) -> bool {
+    let (lo, hi, letter, password) = parse_password_line(line);
+    let password = password.as_bytes();
+    if lo > 0 && lo <= password.len() && hi > 0 && hi <= password.len() {
+        let lo = lo - 1;
+        let hi = hi - 1;
+        let at_lo = password[lo] == letter as u8;
+        let at_hi = password[hi] == letter as u8;
+        at_lo != at_hi
+    } else {
+        false
+    }
 }
 
 fn parse_password_line(line: &str) -> (usize,usize,char,&str) {
@@ -30,15 +44,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_one_line() {
-        assert!(line_valid_password("1-3 a: abcde"));
-        assert!(!line_valid_password("1-3 b: cdefg"));
-        assert!(line_valid_password("2-9 c: ccccccccc"));
-        assert!(!line_valid_password("2-9 c: cccccccccc"));
+    fn test_one_line_1() {
+        assert!(line_valid_password_1("1-3 a: abcde"));
+        assert!(!line_valid_password_1("1-3 b: cdefg"));
+        assert!(line_valid_password_1("2-9 c: ccccccccc"));
+        assert!(!line_valid_password_1("2-9 c: cccccccccc"));
     }
 
     #[test]
     fn test_part_1() {
         assert_eq!(solve_1().unwrap(), "550");
+    }
+
+    #[test]
+    fn test_one_line_2() {
+        assert!(line_valid_password_2("1-3 a: abcde"));
+        assert!(!line_valid_password_2("1-3 b: cdefg"));
+        assert!(!line_valid_password_2("2-9 c: ccccccccc"));
     }
 }
