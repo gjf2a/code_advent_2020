29b931b83fa12a20f0308e9c38dc551e3b2e2518
gjf2a <ferrer@hendrix.edu>
2020-12-04 07:13:37 -0600

    Day 4: I can read the input file into a data structure.

---
+++ src/day4.rs
@@ -0,0 +1,58 @@
+use std::io;
+use crate::for_each_line;
+use std::collections::BTreeMap;
+
+fn fields_and_values_from(filename: &str) -> io::Result<Vec<BTreeMap<String,String>>> {
+    let mut result = Vec::new();
+    let mut current = BTreeMap::new();
+    for_each_line(filename, |line| Ok({
+        let line = line.trim();
+        if line.len() == 0 {
+            result.push(current.clone());
+            current = BTreeMap::new();
+        } else {
+            for pair in line.split_whitespace() {
+                let mut parts = pair.split(':');
+                let key = parts.next().unwrap();
+                let value = parts.next().unwrap();
+                current.insert(key.to_string(), value.to_string());
+            }
+        }
+    }))?;
+
+    result.push(current);
+    Ok(result)
+}
+
+fn stringify(lit: Vec<BTreeMap<&str,&str>>) -> Vec<BTreeMap<String,String>> {
+    let mut result = Vec::new();
+    for m in lit.iter() {
+        let mut ified = BTreeMap::new();
+        for (k, v) in m.iter() {
+            ified.insert(k.to_string(), v.to_string());
+        }
+        result.push(ified);
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn map_test() {
+        let example_target = stringify(vec![
+            btreemap!("ecl"=>"gry", "pid"=>"860033327", "eyr"=>"2020",
+            "hcl"=>"#fffffd", "byr"=>"1937", "iyr"=>"2017", "cid"=>"147", "hgt"=>"183cm"),
+            btreemap!("iyr"=>"2013", "ecl"=>"amb", "cid"=>"350", "eyr"=>"2023",
+            "pid"=>"028048884", "hcl"=>"#cfa07d", "byr"=>"1929"),
+            btreemap!("hcl"=>"#ae17e1", "iyr"=>"2013", "eyr"=>"2024", "ecl"=>"brn",
+            "pid"=>"760753108", "byr"=>"1931", "hgt"=>"179cm"),
+            btreemap!("hcl"=>"#cfa07d", "eyr"=>"2025", "pid"=>"166559648",
+            "iyr"=>"2011", "ecl"=>"brn", "hgt"=>"59in")
+        ]);
+
+        assert_eq!(fields_and_values_from("day_4_example.txt").unwrap(), example_target);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#[macro_use] extern crate maplit;
+
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 use std::{env,fs,io};
 use std::io::BufRead;
