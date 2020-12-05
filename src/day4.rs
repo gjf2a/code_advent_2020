9d5c9811cedd42db574f90e9bb14c8f6d1f86e42
gjf2a <ferrer@hendrix.edu>
2020-12-05 12:00:32 -0600

    Refactored Day 4.

--- src/day4.rs
+++ src/day4.rs
@@ -11,9 +11,7 @@ pub fn solve_2(filename: &str) -> io::Result<String> {
 }
 
 fn solve<P: Fn(&BTreeMap<String,String>) -> bool>(filename: &str, predicate: P) -> io::Result<String> {
-    let count = fields_and_values_from(filename)?.iter()
-        .filter(|m| predicate(*m))
-        .count();
+    let count = fields_and_values_from(filename)?.count_matching(predicate);
     Ok(count.to_string())
 }
 
@@ -73,26 +71,41 @@ fn in_range(value: &str, min: usize, max: usize) -> bool {
     }
 }
 
-fn fields_and_values_from(filename: &str) -> io::Result<Vec<BTreeMap<String,String>>> {
-    let mut result = Vec::new();
-    let mut current = BTreeMap::new();
-    for_each_line(filename, |line| Ok({
+struct Passports {
+    passports: Vec<BTreeMap<String,String>>
+}
+
+impl Passports {
+    pub fn new() -> Self {Passports {passports: vec![BTreeMap::new()]}}
+
+    pub fn add_line(&mut self, line: &str) {
         let line = line.trim();
         if line.len() == 0 {
-            result.push(current.clone());
-            current = BTreeMap::new();
+            self.passports.push(BTreeMap::new());
         } else {
             for pair in line.split_whitespace() {
                 let mut parts = pair.split(':');
                 let key = parts.next().unwrap();
                 let value = parts.next().unwrap();
-                current.insert(key.to_owned(), value.to_owned());
+                let end = self.passports.len() - 1;
+                self.passports[end].insert(key.to_owned(), value.to_owned());
             }
         }
-    }))?;
+    }
 
-    result.push(current);
-    Ok(result)
+    pub fn count_matching<P: Fn(&BTreeMap<String,String>) -> bool>(&self, predicate: P) -> usize {
+        self.passports.iter()
+            .filter(|m| predicate(*m))
+            .count()
+    }
+}
+
+fn fields_and_values_from(filename: &str) -> io::Result<Passports> {
+    let mut passports = Passports::new();
+    for_each_line(filename, |line| Ok({
+        passports.add_line(line);
+    }))?;
+    Ok(passports)
 }
 
 #[cfg(test)]
@@ -120,7 +133,7 @@ mod tests {
             "iyr"=>"2011", "ecl"=>"brn", "hgt"=>"59in")
         ]);
 
-        assert_eq!(fields_and_values_from("day_4_example.txt").unwrap(), example_target);
+        assert_eq!(fields_and_values_from("day_4_example.txt").unwrap().passports, example_target);
     }
 
     #[test]
