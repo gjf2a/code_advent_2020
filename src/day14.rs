b4785cd4c801bbb71938c4314809f34c3a2a063d
gjf2a <ferrer@hendrix.edu>
2020-12-14 08:27:28 -0600

    Day 14: Introduced a trait to abstract the solver.

--- src/day14.rs
+++ src/day14.rs
@@ -3,37 +3,31 @@ use advent_code_lib::all_lines;
 use std::collections::BTreeMap;
 
 pub fn solve_1(filename: &str) -> io::Result<String> {
-    let lines = all_lines(filename)?.map(|line| line.unwrap());
-    let mut mask = Mask1::from("");
-    let mut mem = BTreeMap::new();
-    lines.for_each(|line| {
-        if line.starts_with("mask") {
-            mask = Mask1::from(line.as_str());
-        } else {
-            let (idx, val) = split_mem(line.as_str());
-            mem.insert(idx, mask.mask(val));
-        }
-    });
-    let mem_sum: u64 = mem.values().sum();
-    Ok(mem_sum.to_string())
+    Mask1::from("").solve(filename)
 }
 
 pub fn solve_2(filename: &str) -> io::Result<String> {
-    let lines = all_lines(filename)?.map(|line| line.unwrap());
-    let mut mask = Mask2::from("");
-    let mut mem = BTreeMap::new();
-    lines.for_each(|line| {
-        if line.starts_with("mask") {
-            mask = Mask2::from(line.as_str());
-        } else {
-            let (idx, val) = split_mem(line.as_str());
-            for option in mask.all_variants_of(idx) {
-                mem.insert(option, val);
+    Mask2::from("").solve(filename)
+}
+
+pub trait Solver {
+    fn update_mask(&mut self, line: &str);
+    fn update_mem(&self, idx: u64, val: u64, mem: &mut BTreeMap<u64,u64>);
+
+    fn solve(&mut self, filename: &str) -> io::Result<String> {
+        let lines = all_lines(filename)?.map(|line| line.unwrap());
+        let mut mem = BTreeMap::new();
+        lines.for_each(|line| {
+            if line.starts_with("mask") {
+                self.update_mask(line.as_str());
+            } else {
+                let (idx, val) = split_mem(line.as_str());
+                self.update_mem(idx, val, &mut mem);
             }
-        }
-    });
-    let mem_sum: u64 = mem.values().sum();
-    Ok(mem_sum.to_string())
+        });
+        let mem_sum: u64 = mem.values().sum();
+        Ok(mem_sum.to_string())
+    }
 }
 
 fn split_mem(line: &str) -> (u64, u64) {
@@ -49,12 +43,28 @@ struct Mask1 {
     off: u64
 }
 
+impl Solver for Mask1 {
+    fn update_mask(&mut self, line: &str) {
+        self.replace_from(line);
+    }
+
+    fn update_mem(&self, idx: u64, val: u64, mem: &mut BTreeMap<u64, u64>) {
+        mem.insert(idx, self.mask(val));
+    }
+}
+
 impl Mask1 {
     pub fn from(line: &str) -> Self {
         let mut m = Mask1 { on: 0, off: 0 };
-        line.chars().skip_while(|c| "mask = ".contains(*c))
-            .for_each(|c| m.add(c));
+        m.replace_from(line);
         m
+    }
+
+    pub fn replace_from(&mut self, line: &str) {
+        self.on = 0;
+        self.off = 0;
+        line.chars().skip_while(|c| "mask = ".contains(*c))
+            .for_each(|c| self.add(c));
     }
 
     pub fn add(&mut self, c: char) {
@@ -81,24 +91,41 @@ struct Mask2 {
     versions: Vec<Mask1>
 }
 
+impl Solver for Mask2 {
+    fn update_mask(&mut self, line: &str) {
+        self.replace_from(line);
+    }
+
+    fn update_mem(&self, idx: u64, val: u64, mem: &mut BTreeMap<u64, u64>) {
+        for option in self.all_variants_of(idx) {
+            mem.insert(option, val);
+        }
+    }
+}
+
 impl Mask2 {
     pub fn from(line: &str) -> Self {
-        let mut mask = Mask2 {versions: vec![Mask1::from("")]};
+        let mut mask = Mask2 {versions: Vec::new()};
+        mask.replace_from(line);
+        mask
+    }
+
+    pub fn replace_from(&mut self, line: &str) {
+        self.versions = vec![Mask1::from("")];
         line.chars().skip_while(|c| "mask = ".contains(*c))
             .for_each(|c| {
                 match c {
-                    '0' => mask.add_to_all('X'),
-                    '1' => mask.add_to_all('1'),
+                    '0' => self.add_to_all('X'),
+                    '1' => self.add_to_all('1'),
                     'X' => {
-                        let mut copy = mask.versions.clone();
+                        let mut copy = self.versions.clone();
                         add_to_all(&mut copy, '0');
-                        mask.add_to_all('1');
-                        mask.versions.append(&mut copy);
+                        self.add_to_all('1');
+                        self.versions.append(&mut copy);
                     }
                     _ => panic!("Error! char '{}' unknown", c)
                 }
             });
-        mask
     }
 
     fn add_to_all(&mut self, c: char) {
@@ -152,5 +179,15 @@ mod tests {
             variants.sort();
             assert_eq!(variants, *target);
         }
+    }
+
+    #[test]
+    fn test_solve_1() {
+        assert_eq!(solve_1("day14.txt").unwrap(), "17481577045893");
+    }
+
+    #[test]
+    fn test_solve_2() {
+        assert_eq!(solve_2("day14.txt").unwrap(), "4160009892257");
     }
 }
