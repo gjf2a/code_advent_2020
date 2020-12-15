3e1abca33f460647bc8d7d6b8e6fdf6fd6263986
gjf2a <ferrer@hendrix.edu>
2020-12-15 10:10:00 -0600

    Day 15: Refactored main loop.

--- src/day15.rs
+++ src/day15.rs
@@ -1,30 +1,44 @@
 use std::collections::HashMap;
 
 pub fn solve_1() -> String {
-    elf_1(&PUZZLE_INPUT, 2020).to_string()
+    ElfGame::elf(&PUZZLE_INPUT, 2020).to_string()
 }
 
 pub fn solve_2() -> String {
-    elf_1(&PUZZLE_INPUT, 30000000).to_string()
+    ElfGame::elf(&PUZZLE_INPUT, 30000000).to_string()
 }
 
-pub fn elf_1(starting_nums: &[usize], nth: usize) -> usize {
-    let mut num2last = HashMap::new();
-    let mut spoken = 0;
-    for i in 0..nth {
-        spoken = if i < starting_nums.len() {
+struct ElfGame {
+    num2last: HashMap<usize,(usize,usize)>,
+    spoken: usize
+}
+
+impl ElfGame {
+    pub fn elf(starting_nums: &[usize], nth: usize) -> usize {
+        let mut game = ElfGame {num2last: HashMap::new(), spoken: 0};
+        for i in 0..nth {
+            game.update_spoken(i, starting_nums);
+            game.update_entry(i);
+        }
+        game.spoken
+    }
+
+    fn update_spoken(&mut self, i: usize, starting_nums: &[usize]) {
+        self.spoken = if i < starting_nums.len() {
             starting_nums[i]
         } else {
-            let (turn, prev) = num2last.get(&spoken).unwrap();
+            let (turn, prev) = self.num2last.get(&self.spoken).unwrap();
             turn - prev
         };
-        if let Some(entry) = num2last.get_mut(&spoken) {
+    }
+
+    fn update_entry(&mut self, i: usize) {
+        if let Some(entry) = self.num2last.get_mut(&self.spoken) {
             *entry = (i, entry.0);
         } else {
-            num2last.insert(spoken, (i, i));
+            self.num2last.insert(self.spoken, (i, i));
         }
     }
-    spoken
 }
 
 const PUZZLE_INPUT: [usize; 7] = [2,0,1,7,4,14,18];
@@ -44,7 +58,7 @@ mod tests {
             ([3,2,1], 438),
             ([3,1,2], 1836)
         ].iter() {
-            assert_eq!(elf_1(nums, 2020), *target);
+            assert_eq!(ElfGame::elf(nums, 2020), *target);
         }
     }
 }
