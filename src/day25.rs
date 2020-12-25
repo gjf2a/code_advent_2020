c878e1c4c64ebb0ee2ede98183e0a69d208748c2
gjf2a <ferrer@hendrix.edu>
2020-12-25 00:12:38 -0600

    Day 25: Cleaned up code a bit.

--- src/day25.rs
+++ src/day25.rs
@@ -1,26 +1,26 @@
 use std::mem;
 use num::Integer;
 
+const PUBLIC_SUBJECT: i128 = 7;
+const LOOP_MOD: i128 = 20_201_227;
+
 pub fn solve_1() -> String {
     find_encryption_key(19241437, 17346587).to_string()
 }
 
 fn find_encryption_key(card_public: i128, door_public: i128) -> i128 {
-    let card_loop = find_loop_size(&card_public);
-    let door_loop = find_loop_size(&door_public);
-    println!("Loop sizes: {} {}", card_loop, door_loop);
+    let card_loop = find_loop_size(card_public);
+    let door_loop = find_loop_size(door_public);
     let guess1 = guess_encryption_key(card_loop, &door_public);
     let guess2 = guess_encryption_key(door_loop, &card_public);
     assert_eq!(guess1, guess2);
     guess1
 }
 
-fn find_loop_size(public_key: &i128) -> usize {
-    let public_subject = i128::from(7);
-
-    Transform::from(public_subject.clone())
+fn find_loop_size(public_key: i128) -> usize {
+    Transform::from(PUBLIC_SUBJECT)
         .enumerate()
-        .find(|(_, n)| n == public_key)
+        .find(|(_, n)| n == &public_key)
         .map(|(c,_)| c)
         .unwrap()
 }
@@ -45,8 +45,7 @@ impl Iterator for Transform {
     type Item = i128;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let loop_mod = i128::from(20_201_227);
-        let mut future = (&self.value * &self.subject_number).mod_floor(&loop_mod);
+        let mut future = (&self.value * &self.subject_number).mod_floor(&LOOP_MOD);
         mem::swap(&mut future, &mut self.value);
         Some(future)
     }
@@ -58,8 +57,8 @@ mod tests {
 
     #[test]
     fn test_loop_size() {
-        assert_eq!(find_loop_size(&5764801), 8);
-        assert_eq!(find_loop_size(&17807724), 11);
+        assert_eq!(find_loop_size(5764801), 8);
+        assert_eq!(find_loop_size(17807724), 11);
     }
 
     #[test]
